079ca36e075c10bde8b639d5c5640b3cb664642b
jlw31 <jlw31@bath.ac.uk>
2020-06-16 22:58:33 +0100

    changing data file

--- exp_CT_random_walk_subsampling.py
+++ exp_CT_random_walk_subsampling.py
@@ -14,11 +14,12 @@ from skimage import io
 overwrite = True
 
 directory = '/mnt/jlw31-XDrive/BIMI/ResearchProjects/MJEhrhardt/RC-MA1244_Faraday'
-data_path = directory + '/Data/04-20_CT_Paul_Quinn/phase/sino_cleaned/sino_0050.tif'
+#data_path = directory + '/Data/04-20_CT_Paul_Quinn/phase/sino_cleaned/sino_0050.tif'
+data_path = directory + '/Experiments/CT_diamond/sino_0050_cleaned.png'
 
 data = np.array(io.imread(data_path), dtype=float)
 #data /= np.amax(np.abs(data)) # maybe not te correct way to normalise...
-data = (data - np.amin(data))/(np.amax(data) - np.amin(data))
+#data = (data - np.amin(data))/(np.amax(data) - np.amin(data))
 
 
 height, width = data.shape
