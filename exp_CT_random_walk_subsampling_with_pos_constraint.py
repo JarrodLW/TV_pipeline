130c3038f554f737d934af2f6b2f18f52c1e2dea
jlw31 <jlw31@bath.ac.uk>
2020-07-06 11:32:09 +0100

    commenting out experiment file

--- exp_CT_random_walk_subsampling_with_pos_constraint.py
+++ exp_CT_random_walk_subsampling_with_pos_constraint.py
@@ -44,9 +44,6 @@ data = np.zeros((3, *data_1.shape))
 data[0, :, :] = data_0
 data[1, :, :] = data_1
 data[2, :, :] = data_2
-#data /= np.amax(np.abs(data)) # maybe not te correct way to normalise...
-#data = (data - np.amin(data))/(np.amax(data) - np.amin(data))
-
 
 _, height, width = data.shape
 
@@ -54,47 +51,47 @@ reg_types = ['TV', 'TGV']
 sample_rates = [0.1 * (a + 1) for a in range(10)]
 reg_params = [10 ** (-a) for a in range(10)]
 
-for reg_type in reg_types:
-
-    model = VariationalRegClass('CT', reg_type)
-
-    for sample_rate in sample_rates:
-        num_walks = round(sample_rate*height)
-        subsampling_matrix = horiz_rand_walk_mask(height, width, num_walks, allowing_inter=True, p=[0, 1., 0.])[0]
-
-        for reg_param in reg_params:
-
-            folder = directory + '/Experiments/CT_diamond/' + str(reg_type)+'_regularised_recons/recon_sample_rate_' \
-                     + str(round(sample_rate, 4)) + "_reg_param_" + str(reg_param)
-            #filename = +folder
-
-            recon_numbers = ['0049', '0050', '0051']
-
-            if os.path.isfile(folder + '/recon_' + str(recon_numbers[-1]) + '_with_pos_constraint.png') and not overwrite:
-                continue
-
-            if not os.path.isdir(folder):
-                os.system('mkdir '+ folder)
-
-            recons = model.regularised_recons_from_subsampled_data(data, reg_param, subsampling_arr=mask*subsampling_matrix,
-                                                                   recon_dims=(167, 167), niter=500, a_offset=0, enforce_positivity=True,
-                                                                   a_range=2*np.pi, d_offset=0, d_width=40)
-
-            for i, recon_number in enumerate(recon_numbers):
-
-                np.save(folder + '/recon_array_'+recon_number+'_with_pos_constraint.npy', recons[i])
-
-                plt.figure()
-                plt.imshow(circle_mask(167, 0.95)*recons[i], cmap=plt.cm.gray)
-                plt.axis("off")
-                plt.savefig(folder + '/masked_recon_' + recon_number + '_with_pos_constraint.png')
-                plt.close()
-
-                plt.figure()
-                plt.imshow(recons[i], cmap=plt.cm.gray)
-                plt.colorbar()
-                plt.savefig(folder + '/recon_' + recon_number + '_with_pos_constraint.png')
-                plt.close()
+# for reg_type in reg_types:
+#
+#     model = VariationalRegClass('CT', reg_type)
+#
+#     for sample_rate in sample_rates:
+#         num_walks = round(sample_rate*height)
+#         subsampling_matrix = horiz_rand_walk_mask(height, width, num_walks, allowing_inter=True, p=[0, 1., 0.])[0]
+#
+#         for reg_param in reg_params:
+#
+#             folder = directory + '/Experiments/CT_diamond/' + str(reg_type)+'_regularised_recons/recon_sample_rate_' \
+#                      + str(round(sample_rate, 4)) + "_reg_param_" + str(reg_param)
+#             #filename = +folder
+#
+#             recon_numbers = ['0049', '0050', '0051']
+#
+#             if os.path.isfile(folder + '/recon_' + str(recon_numbers[-1]) + '_with_pos_constraint.png') and not overwrite:
+#                 continue
+#
+#             if not os.path.isdir(folder):
+#                 os.system('mkdir '+ folder)
+#
+#             recons = model.regularised_recons_from_subsampled_data(data, reg_param, subsampling_arr=mask*subsampling_matrix,
+#                                                                    recon_dims=(167, 167), niter=500, a_offset=0, enforce_positivity=True,
+#                                                                    a_range=2*np.pi, d_offset=0, d_width=40)
+#
+#             for i, recon_number in enumerate(recon_numbers):
+#
+#                 np.save(folder + '/recon_array_'+recon_number+'_with_pos_constraint.npy', recons[i])
+#
+#                 plt.figure()
+#                 plt.imshow(circle_mask(167, 0.95)*recons[i], cmap=plt.cm.gray)
+#                 plt.axis("off")
+#                 plt.savefig(folder + '/masked_recon_' + recon_number + '_with_pos_constraint.png')
+#                 plt.close()
+#
+#                 plt.figure()
+#                 plt.imshow(recons[i], cmap=plt.cm.gray)
+#                 plt.colorbar()
+#                 plt.savefig(folder + '/recon_' + recon_number + '_with_pos_constraint.png')
+#                 plt.close()
 
 # generating summary slides
 recon_numbers = ['0049', '0050', '0051']
