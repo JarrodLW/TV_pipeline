30891baee03481bdcb664b133a5e49637508b419
jlw31 <jlw31@bath.ac.uk>
2020-07-07 20:50:09 +0100

    updating astra recon

--- exp_CT_RW_subsampling_with_pos_constraint_skimage.py
+++ exp_CT_RW_subsampling_with_pos_constraint_skimage.py
@@ -83,7 +83,7 @@ for reg_type in reg_types:
                 os.system('mkdir '+ folder)
 
             recons = model.regularised_recons_from_subsampled_data(data, reg_param, subsampling_arr=mask*subsampling_matrix,
-                                                                   recon_dims=(167, 167), niter=1000, a_offset=0, enforce_positivity=True,
+                                                                   recon_dims=(167, 167), niter=200, a_offset=0, enforce_positivity=True,
                                                                    a_range=2*np.pi, d_offset=0, d_width=40)
 
             for i, recon_number in enumerate(recon_numbers):
