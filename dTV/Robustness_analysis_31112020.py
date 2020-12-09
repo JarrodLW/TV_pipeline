4d79b14c3399b45cfb7005efa4702e1a9e5fc28d
jlw31 <jlw31@bath.ac.uk>
2020-12-09 17:03:15 +0000

    adding statistical and analysis files

--- dTV/Robustness_analysis_31112020.py
+++ dTV/Robustness_analysis_31112020.py
@@ -2,27 +2,28 @@ import numpy as np
 import json
 import matplotlib.pyplot as plt
 
-with open('Results_MRI_dTV/Robustness_31112020_TV_512.json') as f:
-    d = json.load(f)
-
+avgs = ['512', '1024', '2048', '4096', '8192', '16384']
 reg_params = np.logspace(np.log10(2e3), np.log10(1e5), num=20)
 output_dims = [int(32), int(64)]
 
-for output_dim in output_dims:
-    for reg_param in reg_params:
-        fig, axs = plt.subplots(8, 4, figsize=(5, 4))
-        for i in range(32):
+for k, avg in enumerate(avgs):
 
-            recon = np.asarray(d['measurement=' + str(i)]['reg_param=' + '{:.1e}'.format(reg_param)]
-                               ['output_size=' + str(output_dim)]).astype('float64')
-            image = np.abs(recon[0] + 1j*recon[1])
+    with open('Results_MRI_dTV/Robustness_31112020_TV_' + avg + '.json') as f:
+        d = json.load(f)
 
-            #image_rotated_flipped = image[:, ::-1].T[:, ::-1]
+    for output_dim in output_dims:
+        for reg_param in reg_params:
+            fig, axs = plt.subplots(8, 4, figsize=(5, 4))
+            for i in range(32):
 
-            axs[i//4, i % 4].imshow(image, cmap=plt.cm.gray)
-            axs[i//4, i % 4].axis("off")
+                recon = np.asarray(d['measurement=' + str(i)]['reg_param=' + '{:.1e}'.format(reg_param)]
+                                   ['output_size=' + str(output_dim)]).astype('float64')
+                image = np.abs(recon[0] + 1j*recon[1])
 
-        fig.tight_layout(w_pad=0.4, h_pad=0.4)
-        plt.savefig("7Li_1H_MRI_Data_31112020/TV_31112020_data_512_avgs_32_to_" + str(
-            output_dim) + "reg_param_" + '{:.1e}'.format(reg_param) + ".pdf")
-        plt.close()
+                axs[i//4, i % 4].imshow(image, cmap=plt.cm.gray)
+                axs[i//4, i % 4].axis("off")
+
+            fig.tight_layout(w_pad=0.4, h_pad=0.4)
+            plt.savefig("7Li_1H_MRI_Data_31112020/TV_31112020_data_" + avg + "_avgs_32_to_" + str(
+                output_dim) + "reg_param_" + '{:.1e}'.format(reg_param) + ".pdf")
+            plt.close()
