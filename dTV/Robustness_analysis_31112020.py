796fe86dda9484468954e26be43fbe995b934af1
jlw31 <jlw31@bath.ac.uk>
2020-12-10 10:10:43 +0000

    modifying analysis file

--- dTV/Robustness_analysis_31112020.py
+++ dTV/Robustness_analysis_31112020.py
@@ -2,7 +2,7 @@ import numpy as np
 import json
 import matplotlib.pyplot as plt
 
-avgs = ['512', '1024', '2048', '4096', '8192', '16384']
+avgs = ['512', '1024', '2048', '4096', '8192']
 reg_params = np.logspace(np.log10(2e3), np.log10(1e5), num=20)
 output_dims = [int(32), int(64)]
 
