03b8924e0d90195bba889ab601dc209427901817
jlw31 <jlw31@bath.ac.uk>
2020-12-10 15:42:56 +0000

    not shifting data fftshift

--- dTV/Robustness_analysis_31112020.py
+++ dTV/Robustness_analysis_31112020.py
@@ -74,11 +74,11 @@ for avg in avgs:
                 #diff = diff[0].asarray() + 1j * diff[1].asarray()
                 #diff_shift = np.fft.ifftshift(diff)
 
-                axs[2*i // 4, i % 4].imshow(np.fft.fftshift(np.abs(synth_data.asarray()[0] + 1j*synth_data.asarray()[1])), cmap=plt.cm.gray)
-                axs[2*i // 4, i % 4].axis("off")
+                axs[2*(i // 4), i % 4].imshow(np.fft.fftshift(np.abs(synth_data.asarray()[0] + 1j*synth_data.asarray()[1])), cmap=plt.cm.gray)
+                axs[2*(i // 4), i % 4].axis("off")
 
-                axs[1+2 * i // 4, i % 4].imshow(np.fft.fftshift(np.abs(data)), cmap=plt.cm.gray)
-                axs[1+2 * i // 4, i % 4].axis("off")
+                axs[1+2 * (i // 4), i % 4].imshow(np.fft.fftshift(np.abs(data)), cmap=plt.cm.gray)
+                axs[1+2 * (i // 4), i % 4].axis("off")
 
             np.save("7Li_1H_MRI_Data_31112020/norms_"+str(output_dim), diff_norms)
 
