846feab56e26321681221a20b1553b4b0f454080
jlw31 <jlw31@bath.ac.uk>
2021-01-12 14:42:00 +0000

    restriscting general file to test saving

--- dTV/Robustness_tests_TV_upsampling_general.py
+++ dTV/Robustness_tests_TV_upsampling_general.py
@@ -47,7 +47,8 @@ if n !=512:
     f_coeff_list = f_coeff_list_grouped
 
 reg_params = np.concatenate((np.asarray([0.001, 1., 10**0.5, 10., 10**1.5, 10**2]), np.logspace(3., 4.5, num=20)))
-output_dims = [int(32), int(64)]
+#output_dims = [int(32), int(64)]
+output_dims = [int(32)]
 #Li_fourier_coeffs = f_coeff_list
 Li_fourier_coeffs = [f_coeff_list[0]]
 
