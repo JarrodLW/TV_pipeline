d9eea21b3e57506ee67cd45e9943bf4b66e3fde6
jlw31 <jlw31@bath.ac.uk>
2021-03-22 17:42:36 +0000

    adding debugging lines to dTV plotting

--- dTV/MRI_15032021/Analysis.py
+++ dTV/MRI_15032021/Analysis.py
@@ -458,8 +458,9 @@ if plot_dTV_results:
                     # example data, just to check consistency of fftshifts etc....
                     if j == 2 and output_dim == int(32) and alpha == alphas[15] and i == 5:
                         data_array = np.asarray(
-                            [fourier_diff, coeffs_minus_GT[i, :, :],
-                             coeffs_minus_GT_TV[i, :, :]])
+                            [fourier_diff, [np.real(coeffs_minus_GT[i, :, :]), np.imag(coeffs_minus_GT[i, :, :])],
+                             [np.real(coeffs_minus_GT_TV[i, :, :]), np.imag(coeffs_minus_GT_TV[i, :, :])],
+                             [GT_TV_data[0], GT_TV_data[1]]])
 
                         np.save('dTV/Results_MRI_dTV/dTV_debugging_fft_shifts.npy', data_array)
 
