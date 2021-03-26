541b7119da84fe1d1b62cfb0b90a0936d09ab3e0
jlw31 <jlw31@bath.ac.uk>
2021-03-26 18:14:48 +0000

    correcting alpha

--- dTV/MRI_15032021/Analysis.py
+++ dTV/MRI_15032021/Analysis.py
@@ -924,7 +924,7 @@ if best_recons:
 
         recon_dTV_32 = np.asarray(d_dTV['measurement=' + str(i)]['output_size=' + str(32)][
                                'alpha=' + '{:.1e}'.format(3.7*10**3)]['recon']).astype('float64')
-        image_dTV_32 = np.abs(recon[0] + 1j * recon[1])
+        image_dTV_32 = np.abs(recon_dTV_32[0] + 1j * recon_dTV_32[1])
 
         recon_dTV_128 = np.asarray(d_dTV['measurement=' + str(i)]['output_size=' + str(128)][
                                       'alpha=' + '{:.1e}'.format(3.7 * 10 ** 3)]['recon']).astype('float64')
