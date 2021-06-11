2535365284934d21af1f984caa59104a8b9daa93
jlw31 <jlw31@bath.ac.uk>
2021-06-11 19:04:15 +0100

    plotting

--- dTV/MRI_15032021/dTV_recons_from_32768_averages.py
+++ dTV/MRI_15032021/dTV_recons_from_32768_averages.py
@@ -203,15 +203,16 @@ if plot:
     for i, eta in enumerate(etas):
 
         for j, gamma in enumerate(gammas):
-            if i == 0:
-                axarr[0, j].set_title(r"$\gamma$ = "+str(gamma), fontsize=14, weight="bold")
-
-            if j == 0:
-                axarr[2*i, j].set_ylabel(r"$\eta$ = "+str(eta), fontsize=14, weight="bold")
 
             axarr[2*i, j].imshow(recon_images[i, j], vmax=np.amax(recon_images), interpolation='none',
                                  cmap=plt.cm.gray)
             axarr[2*i, j].axis("off")
+            if i == 0:
+                axarr[0, j].set_title(r"$\gamma$ = "+str(gamma), fontsize=5, weight="bold")
+
+            if j == 0:
+                axarr[2*i, j].set_ylabel(r"$\eta$ = "+str(eta), fontsize=5, weight="bold")
+
             axarr[2*i+1, j].imshow(fourier_diff_images[i, j], vmax=np.amax(fourier_diff_images), interpolation='none',
                                    cmap=plt.cm.gray)
             axarr[2*i+1, j].axis("off")
