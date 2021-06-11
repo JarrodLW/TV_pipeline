8bab4e6d8fb2cfe4f91395a722e6c9ec316ece46
jlw31 <jlw31@bath.ac.uk>
2021-06-11 19:55:07 +0100

    plotting

--- dTV/MRI_15032021/dTV_recons_from_32768_averages.py
+++ dTV/MRI_15032021/dTV_recons_from_32768_averages.py
@@ -210,7 +210,9 @@ if plot:
             axarr[2 * i, j].axis("off")
             if i == 0:
                 axarr[0, j].set_title(r"$\gamma$ = "+str(gamma), fontsize=5, weight="bold")
-                f.colorbar(plot, ax=axarr[0, j], shrink=0.6)
+
+                if j==-1:
+                    f.colorbar(plot, ax=axarr[0, j], shrink=0.6)
 
             if j == 0:
                 axarr[2*i, 0].text(-0.2, 0.5, r"$\eta$ = "+'{:.1e}'.format(eta), fontsize=5, weight="bold",
