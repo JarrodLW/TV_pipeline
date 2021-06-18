95133c39d9259748cecfe67ffa95ac3eb7de29cc
jlw31 <jlw31@bath.ac.uk>
2021-06-18 14:20:50 +0100

    turning on runexp tag

--- dTV/MRI_15032021/dTV_recons_from_32768_averages.py
+++ dTV/MRI_15032021/dTV_recons_from_32768_averages.py
@@ -21,8 +21,8 @@ from skimage.transform import resize
 alpha = float(sys.argv[1])
 #alpha=600.
 
-run_expt = False
-plot = True
+run_expt = True
+plot = False
 regis = False
 
 Li_fourier = np.fft.fftshift(np.load('dTV/MRI_15032021/Results_24052021/32768_data.npy'))
